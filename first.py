dc12cf0807ae7de8dba8cdd3fda119635bcdba5e
Krish lad <ladkrish258@gmail.com>
2026-07-07 14:09:19 +0530

    Delete first.py

--- first.py
+++
@@ -1,7 +0,0 @@
-n1=int(input("Enter First Number : "))
-n2=int(input("Enter Second Number : "))
-
-print("Addition : ",n1+n2)
-print("Subtraction : ",n1-n2)
-print("Multiplication : ",n1*n2)
-print("Division : ",n1/n2)
